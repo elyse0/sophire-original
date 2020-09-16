42504148d7a3d801af4703b383472fec70afffd1
kolverar <kevincristian@outlook.com>
2020-09-15 19:16:51 -0500

    REST API Documentation

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/verbs', imagesRouter)
+app.use('/api/verbs', imagesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -26,9 +26,40 @@ router.get('/random', function(req, res) {
     });
 });
 
+router.get('/verbs', function(req, res) {
+
+    Verb.find({}, (err, data) => {
+
+        if(err)
+            res.status(500).json({mensaje: "error!"})
+        else
+            res.render('verbs', {verbs: data});
+    });
+});
+
+
 router.get('/admin', function(req, res) {
 
     res.render('admin');
+});
+
+
+router.get('/api', function(req, res) {
+
+    res.render('api');
+});
+
+router.post('/search', function(req, res) {
+
+    Verb.find({name: {'$regex': req.body.search}}, (err, data) => {
+
+        if(err)
+            res.status(404).json({message: "Error!"})
+        else {
+            res.render('search', {verbs: data})
+            console.log(data)
+        }
+    })
 });
 
 module.exports = router;
