4957b301e7e4dd391e866b5b140a980c3e65774b
kolverar <kevincristia@outlook.com>
2021-04-09 01:04:11 -0500

    New repository created called suffire_resources to store all the images
    The main problem was deploying the app to Heroku. From a long time, we've had an alert related to the project size and I think having a different repo for the resouces makes sense because now it's over 700 MB!

--- public/images/random.image.py
+++
@@ -1,6 +0,0 @@
-import os
-import random
-
-path = "/home/amish/Documents/French/Images/Verbes/Images"
-images = os.listdir(path)
-os.system("gthumb " + path + "/"+ images[random.randint(0, len(images))])
