a89d18758f961101af0fd2eac63588257cb584b2
elyse0 <kevincristia@outlook.com>
2021-04-24 18:42:05 -0500

    Add conjugation of the new verb added

--- routes/verbs_api/verbs_api_post.js
+++ routes/verbs_api/verbs_api_post.js
@@ -4,16 +4,18 @@ const axios = require('axios');
 
 // Util
 const apiAuth = require('/util/auth_api')
-const cdn = require('/util/cdn')
+const cdn = require('/util/urls').cdn
 let normalization = require('/util/normalization')
+let post_verb_conjugation = require('/routes/verb_conjugation_api/verb_conjugation_post').post_verb_conjugation
 
 const Verb = require('/models/verb');
 
-const CDN = cdn.URL + "/verbs/"
+const CDN = cdn + "/verbs/"
 
 // POST / - Add a new verb providing nameUTF8 and name
 router.post('/', apiAuth.checkJwt , (req, res) => {
 
+    let name = req.body.name
     let nameUTF8 = normalization.getNormalizedName(req.body.name)
 
     // Check if verb exists
@@ -28,28 +30,35 @@ router.post('/', apiAuth.checkJwt , (req, res) => {
                 .then((response) => {
 
                     let verb = Verb({
-                            name: req.body.name,
+                            name: name,
                             nameUTF8: nameUTF8,
                             imageURL: CDN + nameUTF8 + ".png"
                         }
                     )
 
-                    verb.save((err, data) => {
+                    // Save new verb conjugation
+                    post_verb_conjugation(name)
+                        .then((data) => {
 
-                        if(err)
-                            res.status(404).json({message: "Can't save it!"})
-                        else
-                            res.status(201).json(data)
-                    })
+                            // Save new verb
+                            verb.save((err, data) => {
+
+                                if(err)
+                                    return res.status(404).json({message: "Can't save it!"})
+
+                                return res.status(201).json(data)
+                            })
+                        })
                 })
                 .catch((err) => {
 
-                    res.status(404).json({message: "Can't find image!"})
+                    console.log(err)
+                    return res.status(404).json("Error")
                 })
         }
         else {
             console.log(data)
-            res.status(404).json({message: "Verb already exists!"})
+            return res.status(404).json({message: "Verb already exists!"})
         }
     })
 });
